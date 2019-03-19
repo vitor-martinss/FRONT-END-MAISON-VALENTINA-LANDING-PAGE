fad57f3610b6bdfbc29f9737bea3cdfe9e6ad5bf
Vitor Martins <vitormartinsweb@gmail.com>
2019-03-19 12:20:27 +0000

    Ready for use

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$(".icon-search").click(function(){
+		$(".search-tab").slideToggle();
+	});
+
+});
+ // $(".down1").click(function(){
+ //    $(this).toggleClass("fa-angle-up");
+ //    $(".faqs-mobile").slideToggle();
+ //  });
